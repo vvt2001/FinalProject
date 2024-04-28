72046455910358c84de00547f848b17d1de96135
Vũ Việt Thắng <vuvietthang1508@gmail.com>
2024-04-28 09:35:07 +0700

    Add web project

---
+++ FinalProject_Data/Migrations/20240428023333_add-location-meeting.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FinalProject_Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class addlocationmeeting : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "location",
+                table: "meetings",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "location",
+                table: "meetingforms",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "location",
+                table: "meetings");
+
+            migrationBuilder.DropColumn(
+                name: "location",
+                table: "meetingforms");
+        }
+    }
+}
